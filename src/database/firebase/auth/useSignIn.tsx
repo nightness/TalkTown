87c12b5e9a5c97845d2cf0b6438d147caef360ab
nightness <joshguyette@yahoo.com>
2022-06-27 22:03:01 +0000

    removed used duplicates

--- src/database/firebase/auth/useSignIn.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { signInWithPopup, signInWithRedirect } from 'firebase/auth';
-
-// Redirect's fuck up debugging authentication, use popup's for __DEV__
-const useSignIn = () => __DEV__ ? signInWithPopup : signInWithRedirect;
-
-export default useSignIn
